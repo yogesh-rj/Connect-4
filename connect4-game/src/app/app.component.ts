01ce650f0957fe52a83e9036e58af09feb17db13
yogesh-rj <yougeshjsh846@gmail.com>
2020-10-29 19:16:14 +0530

    added functionality to match 4 discs  in columns

--- connect4-game/src/app/app.component.ts
+++ connect4-game/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'connect4-game';
   player1 = true;
   player2 = false;
+  isMatched = false;
   rows = [
     { 1: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
     { 2: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
@@ -21,28 +22,33 @@ export class AppComponent {
 
   // this method is for selecting Discs
   selectColumn(colNumber) {
-    for (let i = 6; i > 1; i--) {
-      if (this.rows[i - 1][i][colNumber] === 'a' || this.rows[i - 1][i][colNumber] === 'b') {
-        if (this.player1) {
-          this.rows[i - 2][i - 1][colNumber] = 'a';
-        }
-        if (this.player2) {
-          this.rows[i - 2][i - 1][colNumber] = 'b';
-        }
-        this.checkRowCombinations(i - 2, i - 1, colNumber);
-        this.player2 = !this.player2;
-        this.player1 = !this.player1;
-        return;
-      } else {
+    for (let i = 6; i > 0; i--) {
+      if (this.rows[i - 1][i][colNumber] !== 'a' && this.rows[i - 1][i][colNumber] !== 'b') {
         if (this.player1) {
           this.rows[i - 1][i][colNumber] = 'a';
         }
         if (this.player2) {
           this.rows[i - 1][i][colNumber] = 'b';
         }
-        this.checkRowCombinations(i - 1, i, colNumber);
+        const rowMatch = this.checkRowCombinations(i - 1, i, colNumber);
+        const columnMatch = this.checkColumnsCombinations(i - 1, i, colNumber);
         this.player2 = !this.player2;
         this.player1 = !this.player1;
+        if (rowMatch && this.player1) {
+          this.isMatched = true;
+          alert('Player 1 wins!!!');
+        } else if (rowMatch && this.player2) {
+          this.isMatched = true;
+          alert('player 2 wins!!!');
+        }
+
+        if (columnMatch && this.player1) {
+          this.isMatched = true;
+          alert('Player 1 wins!!!');
+        } else if (columnMatch && this.player2) {
+          this.isMatched = true;
+          alert('player 2 wins!!!');
+        }
 
         return;
       }
@@ -59,12 +65,35 @@ export class AppComponent {
     });
     const re = new RegExp(/a{4}|b{4}/);
     rowMatch = re.test(rowStr);
-    if (rowMatch && this.player1) {
-      alert('Player 1 wins!!!');
-    } else if (rowMatch && this.player2) {
-      alert('player 2 wins!!!');
-    }
     console.log(rowMatch);
+    return rowMatch;
+  }
+
+  // this method will check for column combinations
+  checkColumnsCombinations(index, rownumber, columNumber) {
+    let columnMatch;
+    let colStr = '';
+    this.rows.forEach((rw, i) => {
+      colStr = colStr + rw[i + 1][columNumber];
+    });
+    const re = new RegExp(/a{4}|b{4}/);
+    columnMatch = re.test(colStr);
+    console.log(columnMatch);
+    return columnMatch;
+  }
+
+  // this method will reset the game
+  resetGame(){
+    this.player1 = true;
+    this.isMatched = false;
+    this.rows = [
+      { 1: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
+      { 2: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
+      { 3: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
+      { 4: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
+      { 5: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } },
+      { 6: { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0', 7: '0' } }
+    ];
   }
 
 }

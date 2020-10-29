43b5a9dc6a8e89c580871a3da59ac9654436a903
yogesh-rj <yougeshjsh846@gmail.com>
2020-10-29 17:52:14 +0530

    added winning combination for row

--- connect4-game/src/app/app.component.ts
+++ connect4-game/src/app/app.component.ts
@@ -22,32 +22,49 @@ export class AppComponent {
   // this method is for selecting Discs
   selectColumn(colNumber) {
     for (let i = 6; i > 1; i--) {
-      if (this.rows[i - 1][i][colNumber] === 'p1' || this.rows[i - 1][i][colNumber] === 'p2') {
+      if (this.rows[i - 1][i][colNumber] === 'a' || this.rows[i - 1][i][colNumber] === 'b') {
         if (this.player1) {
-          this.rows[i - 2][i - 1][colNumber] = 'p1';
+          this.rows[i - 2][i - 1][colNumber] = 'a';
         }
         if (this.player2) {
-          this.rows[i - 2][i - 1][colNumber] = 'p2';
+          this.rows[i - 2][i - 1][colNumber] = 'b';
         }
+        this.checkRowCombinations(i - 2, i - 1, colNumber);
         this.player2 = !this.player2;
         this.player1 = !this.player1;
         return;
       } else {
         if (this.player1) {
-          this.rows[i - 1][i][colNumber] = 'p1';
+          this.rows[i - 1][i][colNumber] = 'a';
         }
         if (this.player2) {
-          this.rows[i - 1][i][colNumber] = 'p2';
+          this.rows[i - 1][i][colNumber] = 'b';
         }
-
+        this.checkRowCombinations(i - 1, i, colNumber);
         this.player2 = !this.player2;
         this.player1 = !this.player1;
+
         return;
       }
-
-      console.log(this.rows);
     }
 
+  }
+
+  // this method will check for row combinations
+  checkRowCombinations(index, rownumber, columNumber) {
+    let rowMatch;
+    let rowStr = '';
+    const rowValues = Object.values(this.rows[index][rownumber]).forEach(v => {
+      rowStr = rowStr + v;
+    });
+    const re = new RegExp(/a{4}|b{4}/);
+    rowMatch = re.test(rowStr);
+    if (rowMatch && this.player1) {
+      alert('Player 1 wins!!!');
+    } else if (rowMatch && this.player2) {
+      alert('player 2 wins!!!');
+    }
+    console.log(rowMatch);
   }
 
 }
